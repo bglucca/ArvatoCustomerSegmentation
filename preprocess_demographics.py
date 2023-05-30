fde39cfcd4f6926318af501c2f80c1e583548b23
Lucca Gomes <luccagomes2@gmail.com>
2023-05-30 10:50:02 -0300

    Changes to Adapt to all vars

--- preprocess_demographics.py
+++ preprocess_demographics.py
@@ -23,7 +23,7 @@ def demographic_preprocess_pipeline(input_path = args.input_path,
 
     '''
     Does the preprocessing of the demographic data after Explorations in the 01 and 02 notebooks.
-    Exports data as Parquet to path specified in output_path
+    Exports data as Parquet to path specified in output_path'
 
     :param input_path: demographic data input file path
     :output_path: file export path
@@ -32,17 +32,66 @@ def demographic_preprocess_pipeline(input_path = args.input_path,
 
     data = pd.read_csv(input_path, sep = ';')
 
+    print('Renaming csv data columns to match documentation...')
+
+    d19_cols_rename = {'D19_BANKEN_DIREKT': 'D19_BANKEN_DIREKT_RZ',
+                        'D19_BANKEN_GROSS': 'D19_BANKEN_GROSS_RZ',
+                        'D19_BANKEN_LOKAL': 'D19_BANKEN_LOKAL_RZ',
+                        'D19_BANKEN_REST': 'D19_BANKEN_REST_RZ',
+                        'D19_BEKLEIDUNG_GEH': 'D19_BEKLEIDUNG_GEH_RZ',
+                        'D19_BEKLEIDUNG_REST': 'D19_BEKLEIDUNG_REST_RZ',
+                        'D19_BILDUNG': 'D19_BILDUNG_RZ',
+                        'D19_BIO_OEKO': 'D19_BIO_OEKO_RZ',
+                        'D19_BUCH_CD': 'D19_BUCH_CD_RZ',
+                        'D19_DIGIT_SERV': 'D19_DIGIT_SERV_RZ',
+                        'D19_DROGERIEARTIKEL': 'D19_DROGERIEARTIKEL_RZ',
+                        'D19_ENERGIE': 'D19_ENERGIE_RZ',
+                        'D19_FREIZEIT': 'D19_FREIZEIT_RZ',
+                        'D19_GARTEN': 'D19_GARTEN_RZ',
+                        'D19_HANDWERK': 'D19_HANDWERK_RZ',
+                        'D19_HAUS_DEKO': 'D19_HAUS_DEKO_RZ',
+                        'D19_KINDERARTIKEL': 'D19_KINDERARTIKEL_RZ',
+                        'D19_KONSUMTYP_MAX': 'D19_KONSUMTYP_MAX_RZ',
+                        'D19_KOSMETIK': 'D19_KOSMETIK_RZ',
+                        'D19_LEBENSMITTEL': 'D19_LEBENSMITTEL_RZ',
+                        'D19_LETZTER_KAUF_BRANCHE': 'D19_LETZTER_KAUF_BRANCHE_RZ',
+                        'D19_LOTTO': 'D19_LOTTO_RZ',
+                        'D19_NAHRUNGSERGAENZUNG': 'D19_NAHRUNGSERGAENZUNG_RZ',
+                        'D19_RATGEBER': 'D19_RATGEBER_RZ',
+                        'D19_REISEN': 'D19_REISEN_RZ',
+                        'D19_SAMMELARTIKEL': 'D19_SAMMELARTIKEL_RZ',
+                        'D19_SCHUHE': 'D19_SCHUHE_RZ',
+                        'D19_SONSTIGE': 'D19_SONSTIGE_RZ',
+                        'D19_SOZIALES': 'D19_SOZIALES_RZ',
+                        'D19_TECHNIK': 'D19_TECHNIK_RZ',
+                        'D19_TELKO_MOBILE': 'D19_TELKO_MOBILE_RZ',
+                        'D19_TELKO_REST': 'D19_TELKO_REST_RZ',
+                        'D19_TIERARTIKEL': 'D19_TIERARTIKEL_RZ',
+                        'D19_VERSAND_REST': 'D19_VERSAND_REST_RZ',
+                        'D19_VERSICHERUNGEN': 'D19_VERSICHERUNGEN_RZ',
+                        'D19_VOLLSORTIMENT': 'D19_VOLLSORTIMENT_RZ',
+                        'D19_WEIN_FEINKOST': 'D19_WEIN_FEINKOST_RZ'}
+
+    other_cols_rename = {'CAMEO_INTL_2015':'CAMEO_DEUINTL_2015',
+                        'SOHO_KZ':'SOHO_FLAG',
+                        'KK_KUNDENTYP':'D19_KK_KUNDENTYP',
+                        'KBA13_CCM_1401_2500':'KBA13_CCM_1400_2500'}
+
+    data.rename(columns = d19_cols_rename, inplace = True)
+    
+    data.rename(columns = other_cols_rename, inplace = True)
+
     print('Replacing NaNs...')
     
     data['CAMEO_DEUG_2015'] = data['CAMEO_DEUG_2015'].replace('X',np.nan)
 
-    data['CAMEO_INTL_2015'] = data['CAMEO_INTL_2015'].replace('XX',np.nan)
+    data['CAMEO_DEUINTL_2015'] = data['CAMEO_DEUINTL_2015'].replace('XX',np.nan)
 
     data['CAMEO_DEU_2015'] = data['CAMEO_DEU_2015'].replace('XX',np.nan)
 
     data['CAMEO_DEUG_2015'] = data['CAMEO_DEUG_2015'].fillna(-1).astype(int)
 
-    data['CAMEO_INTL_2015'] = data['CAMEO_INTL_2015'].fillna(-1).astype(int)
+    data['CAMEO_DEUINTL_2015'] = data['CAMEO_DEUINTL_2015'].fillna(-1).astype(int)
 
     data['CAMEO_DEU_2015'] = data['CAMEO_DEU_2015'].fillna('-1')
 
@@ -52,15 +101,74 @@ def demographic_preprocess_pipeline(input_path = args.input_path,
 
     print('Subsetting...')
 
-    if is_customer_data:
+    # if is_customer_data:
 
-        SELECTED_COLS_SUBSET.extend(['CUSTOMER_GROUP','ONLINE_PURCHASE','PRODUCT_GROUP'])
+    #     SELECTED_COLS_SUBSET.extend(['CUSTOMER_GROUP','ONLINE_PURCHASE','PRODUCT_GROUP'])
 
-    if is_mailout_train:
+    # if is_mailout_train:
 
-        SELECTED_COLS_SUBSET.extend(['RESPONSE'])
+    #     SELECTED_COLS_SUBSET.extend(['RESPONSE'])
 
-    data = data[SELECTED_COLS_SUBSET].copy()
+    # data = data[SELECTED_COLS_SUBSET].copy()
+
+    cols_to_drop = ['GEBURTSJAHR',
+                    'SOHO_FLAG',
+                    'KBA13_HALTER_20',
+                    'KBA13_HALTER_25',
+                    'KBA13_HALTER_30',
+                    'KBA13_HALTER_35',
+                    'KBA13_HALTER_40',
+                    'KBA13_HALTER_45',
+                    'KBA13_HALTER_50',
+                    'KBA13_HALTER_55',
+                    'KBA13_HALTER_60',
+                    'KBA13_HALTER_65',
+                    'KBA13_HALTER_66',
+                    'KBA13_CCM_1400_2500',
+                    'KBA13_KMH_110',
+                    'KBA13_KMH_140',
+                    'KBA13_KMH_180',
+                    'KBA13_KMH_210',
+                    'KBA13_KMH_250',
+                    'KBA13_KMH_251',
+                    'ALTERSKATEGORIE_FEIN',
+                    'LP_FAMILIE_FEIN',
+                    'LP_LEBENSPHASE_FEIN',
+                    'LP_STATUS_FEIN']
+    
+    not_found_cols = ['AKT_DAT_KL',
+                    'DSL_FLAG',
+                    'EINGEFUEGT_AM',
+                    'EINGEZOGENAM_HH_JAHR',
+                    'EXTSEL992',
+                    'FIRMENDICHTE',
+                    'GEMEINDETYP',
+                    'HH_DELTA_FLAG',
+                    'KBA13_BAUMAX',
+                    'KBA13_GBZ',
+                    'KBA13_HHZ',
+                    'KOMBIALTER',
+                    'KONSUMZELLE',
+                    'MOBI_RASTER',
+                    'RT_KEIN_ANREIZ',
+                    'RT_SCHNAEPPCHEN',
+                    'RT_UEBERGROESSE',
+                    'STRUKTURTYP',
+                    'UMFELD_ALT',
+                    'UMFELD_JUNG',
+                    'UNGLEICHENN_FLAG',
+                    'VERDICHTUNGSRAUM',
+                    'VHA',
+                    'VHN',
+                    'VK_DHT4A',
+                    'VK_DISTANZ',
+                    'VK_ZG11',
+                    'KBA13_ANTG1',
+                    'KBA13_ANTG2',
+                    'KBA13_ANTG3',
+                    'KBA13_ANTG4']
+    
+    data.drop(columns = cols_to_drop + not_found_cols, inplace = True)
 
     print('Fixing specific columns...')
 
@@ -93,9 +201,9 @@ def demographic_preprocess_pipeline(input_path = args.input_path,
 
     # Imputing using mode (most frequent) for categorical
     for col in cat_cols:
-
-        data[col].fillna(data[col].mode()[0], inplace = True)
         
+        data[col].fillna(data[col].mode().values[0], inplace = True)
+
     num_cols = CENSUS_VAR_TYPES[~CENSUS_VAR_TYPES['Type'].isin(['interval', 'nominal', 'binary'])]['Attribute'].values
 
     # Imputing using mean for numerical
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -3,186 +3,192 @@ import numpy as np
 
 VALUES_PATH = 'data/raw/DIAS Attributes - Values 2017.xlsx'
 
-# Based on the 01 Notebook
-SELECTED_COLS_SUBSET = ['LNR',
-                        'ANZ_HAUSHALTE_AKTIV',
-                        'ANZ_HH_TITEL',
-                        'ANZ_PERSONEN',
-                        'ANZ_TITEL',
-                        'KBA13_ANZAHL_PKW',
-                        'MIN_GEBAEUDEJAHR',
-                        'D19_BANKEN_DIREKT',
-                        'D19_BANKEN_GROSS',
-                        'D19_BANKEN_LOKAL',
-                        'D19_BANKEN_REST',
-                        'D19_BIO_OEKO',
-                        'D19_DIGIT_SERV',
-                        'D19_LEBENSMITTEL',
-                        'D19_VOLLSORTIMENT',
-                        'D19_VERSAND_REST',
-                        'GEBAEUDETYP',
-                        'KONSUMNAEHE',
-                        'OST_WEST_KZ',
-                        'WOHNLAGE',
-                        'KBA05_MOTOR',
-                        'KBA05_FRAU',
-                        'KBA05_KRSZUL',
-                        'KBA05_DIESEL',
-                        'KBA05_KRSKLEIN',
-                        'KBA05_KRSAQUOT',
-                        'KBA05_MAXVORB',
-                        'KBA05_MOTRAD',
-                        'KBA05_MAXAH',
-                        'KBA05_MODTEMP',
-                        'KBA05_KRSOBER',
-                        'KBA05_KRSVAN',
-                        'KBA05_MAXHERST',
-                        'KBA05_MAXBJ',
-                        'ARBEIT',
-                        'ORTSGR_KLS9',
-                        'HH_EINKOMMEN_SCORE',
-                        'D19_KONSUMTYP',
-                        'D19_GESAMT_DATUM',
-                        'D19_BANKEN_DATUM',
-                        'D19_VERSAND_DATUM',
-                        'D19_GESAMT_ONLINE_QUOTE_12',
-                        'D19_BANKEN_ONLINE_QUOTE_12',
-                        'D19_VERSAND_ONLINE_QUOTE_12',
-                        'W_KEIT_KIND_HH',
-                        'WOHNDAUER_2008',
-                        'D19_GESAMT_ANZ_12',
-                        'D19_VERSAND_ANZ_12',
-                        'ANZ_KINDER',
-                        'ANZ_STATISTISCHE_HAUSHALTE',
-                        'STRUKTURTYP',
-                        'CAMEO_DEUG_2015',
-                        'CAMEO_DEU_2015',
-                        'KBA05_ALTER1',
-                        'KBA05_ALTER2',
-                        'KBA05_ALTER3',
-                        'KBA05_ALTER4',
-                        'KBA05_ANHANG',
-                        'KBA05_ANTG1',
-                        'KBA05_ANTG2',
-                        'KBA05_ANTG3',
-                        'KBA05_ANTG4',
-                        'ALTERSKATEGORIE_GROB',
-                        'ANREDE_KZ',
-                        'CJT_GESAMTTYP',
-                        'FINANZ_MINIMALIST',
-                        'FINANZ_SPARER',
-                        'FINANZ_VORSORGER',
-                        'FINANZ_ANLEGER',
-                        'FINANZ_UNAUFFAELLIGER',
-                        'FINANZ_HAUSBAUER',
-                        'FINANZTYP',
-                        'GFK_URLAUBERTYP',
-                        'GREEN_AVANTGARDE',
-                        'HEALTH_TYP',
-                        'LP_LEBENSPHASE_GROB',
-                        'LP_FAMILIE_GROB',
-                        'LP_STATUS_GROB',
-                        'NATIONALITAET_KZ',
-                        'PRAEGENDE_JUGENDJAHRE',
-                        'RETOURTYP_BK_S',
-                        'SEMIO_SOZ',
-                        'SEMIO_FAM',
-                        'SEMIO_REL',
-                        'SEMIO_MAT',
-                        'SEMIO_VERT',
-                        'SEMIO_LUST',
-                        'SEMIO_ERL',
-                        'SEMIO_KULT',
-                        'SEMIO_RAT',
-                        'SEMIO_KRIT',
-                        'SEMIO_DOM',
-                        'SEMIO_KAEM',
-                        'SEMIO_PFLICHT',
-                        'SEMIO_TRADV',
-                        'SHOPPER_TYP',
-                        'VERS_TYP',
-                        'ZABEOTYP',
-                        'CJT_KATALOGNUTZER',
-                        'CJT_TYP_1',
-                        'CJT_TYP_2',
-                        'CJT_TYP_3',
-                        'CJT_TYP_4',
-                        'CJT_TYP_5',
-                        'CJT_TYP_6',
-                        'KBA13_SEG_MINIWAGEN',
-                        'KBA13_CCM_2000',
-                        'KBA13_KMH_0_140',
-                        'KBA13_SEG_SONSTIGE',
-                        'KBA13_VORB_0',
-                        'KBA13_CCM_1200',
-                        'PLZ8_ANTG2',
-                        'KBA13_KMH_211',
-                        'KBA13_CCM_1000',
-                        'PLZ8_ANTG3',
-                        'KBA13_VORB_1_2',
-                        'KBA13_ALTERHALTER_61',
-                        'KBA13_ALTERHALTER_30',
-                        'KBA13_AUTOQUOTE',
-                        'KBA13_SEG_KLEINWAGEN',
-                        'KBA13_CCM_1600',
-                        'KBA13_GBZ',
-                        'KBA13_SEG_WOHNMOBILE',
-                        'KBA13_CCM_2500',
-                        'KBA13_SEG_GROSSRAUMVANS',
-                        'KBA13_SEG_OBEREMITTELKLASSE',
-                        'KBA13_KRSAQUOT',
-                        'PLZ8_GBZ',
-                        'KBA13_VORB_2',
-                        'KBA13_SEG_OBERKLASSE',
-                        'KBA13_BJ_2008',
-                        'KBA13_ALTERHALTER_60',
-                        'PLZ8_ANTG1',
-                        'KBA13_SEG_MITTELKLASSE',
-                        'KBA13_SEG_GELAENDEWAGEN',
-                        'KBA13_BJ_2000',
-                        'KBA13_KRSSEG_KLEIN',
-                        'KBA13_CCM_3000',
-                        'KBA13_SEG_UTILITIES',
-                        'KBA13_SEG_MINIVANS',
-                        'KBA13_VORB_1',
-                        'KBA13_BJ_1999',
-                        'PLZ8_ANTG4',
-                        'KBA13_KRSSEG_OBER',
-                        'KBA13_CCM_3001',
-                        'KBA13_KRSSEG_VAN',
-                        'KBA13_KRSZUL_NEU',
-                        'KBA13_SEG_KOMPAKTKLASSE',
-                        'KBA13_KMH_140_210',
-                        'KBA13_SEG_SPORTWAGEN',
-                        'KBA13_CCM_1400',
-                        'KBA13_BAUMAX',
-                        'PLZ8_BAUMAX',
-                        'PLZ8_HHZ',
-                        'KBA13_BJ_2009',
-                        'KBA13_CCM_2501',
-                        'KBA13_VORB_3',
-                        'KBA13_CCM_1500',
-                        'KBA13_BJ_2006',
-                        'KBA13_HHZ',
-                        'KBA13_CCM_1800',
-                        'KBA13_SEG_KLEINST',
-                        'KBA13_ALTERHALTER_45',
-                        'KBA13_SEG_VAN',
-                        'KBA13_BJ_2004',
-                        'BALLRAUM',
-                        'EWDICHTE',
-                        'INNENSTADT',
-                        'GEBAEUDETYP_RASTER',
-                        'KKK',
-                        'MOBI_REGIO',
-                        'ONLINE_AFFINITAET']
+# # Based on the 01 Notebook
+# SELECTED_COLS_SUBSET = ['LNR',
+#                         'ANZ_HAUSHALTE_AKTIV',
+#                         'ANZ_HH_TITEL',
+#                         'ANZ_PERSONEN',
+#                         'ANZ_TITEL',
+#                         'KBA13_ANZAHL_PKW',
+#                         'MIN_GEBAEUDEJAHR',
+#                         'D19_BANKEN_DIREKT',
+#                         'D19_BANKEN_GROSS',
+#                         'D19_BANKEN_LOKAL',
+#                         'D19_BANKEN_REST',
+#                         'D19_BIO_OEKO',
+#                         'D19_DIGIT_SERV',
+#                         'D19_LEBENSMITTEL',
+#                         'D19_VOLLSORTIMENT',
+#                         'D19_VERSAND_REST',
+#                         'GEBAEUDETYP',
+#                         'KONSUMNAEHE',
+#                         'OST_WEST_KZ',
+#                         'WOHNLAGE',
+#                         'KBA05_MOTOR',
+#                         'KBA05_FRAU',
+#                         'KBA05_KRSZUL',
+#                         'KBA05_DIESEL',
+#                         'KBA05_KRSKLEIN',
+#                         'KBA05_KRSAQUOT',
+#                         'KBA05_MAXVORB',
+#                         'KBA05_MOTRAD',
+#                         'KBA05_MAXAH',
+#                         'KBA05_MODTEMP',
+#                         'KBA05_KRSOBER',
+#                         'KBA05_KRSVAN',
+#                         'KBA05_MAXHERST',
+#                         'KBA05_MAXBJ',
+#                         'ARBEIT',
+#                         'ORTSGR_KLS9',
+#                         'HH_EINKOMMEN_SCORE',
+#                         'D19_KONSUMTYP',
+#                         'D19_GESAMT_DATUM',
+#                         'D19_BANKEN_DATUM',
+#                         'D19_VERSAND_DATUM',
+#                         'D19_GESAMT_ONLINE_QUOTE_12',
+#                         'D19_BANKEN_ONLINE_QUOTE_12',
+#                         'D19_VERSAND_ONLINE_QUOTE_12',
+#                         'W_KEIT_KIND_HH',
+#                         'WOHNDAUER_2008',
+#                         'D19_GESAMT_ANZ_12',
+#                         'D19_VERSAND_ANZ_12',
+#                         'ANZ_KINDER',
+#                         'ANZ_STATISTISCHE_HAUSHALTE',
+#                         'STRUKTURTYP',
+#                         'CAMEO_DEUG_2015',
+#                         'CAMEO_DEU_2015',
+#                         'KBA05_ALTER1',
+#                         'KBA05_ALTER2',
+#                         'KBA05_ALTER3',
+#                         'KBA05_ALTER4',
+#                         'KBA05_ANHANG',
+#                         'KBA05_ANTG1',
+#                         'KBA05_ANTG2',
+#                         'KBA05_ANTG3',
+#                         'KBA05_ANTG4',
+#                         'ALTERSKATEGORIE_GROB',
+#                         'ANREDE_KZ',
+#                         'CJT_GESAMTTYP',
+#                         'FINANZ_MINIMALIST',
+#                         'FINANZ_SPARER',
+#                         'FINANZ_VORSORGER',
+#                         'FINANZ_ANLEGER',
+#                         'FINANZ_UNAUFFAELLIGER',
+#                         'FINANZ_HAUSBAUER',
+#                         'FINANZTYP',
+#                         'GFK_URLAUBERTYP',
+#                         'GREEN_AVANTGARDE',
+#                         'HEALTH_TYP',
+#                         'LP_LEBENSPHASE_GROB',
+#                         'LP_FAMILIE_GROB',
+#                         'LP_STATUS_GROB',
+#                         'NATIONALITAET_KZ',
+#                         'PRAEGENDE_JUGENDJAHRE',
+#                         'RETOURTYP_BK_S',
+#                         'SEMIO_SOZ',
+#                         'SEMIO_FAM',
+#                         'SEMIO_REL',
+#                         'SEMIO_MAT',
+#                         'SEMIO_VERT',
+#                         'SEMIO_LUST',
+#                         'SEMIO_ERL',
+#                         'SEMIO_KULT',
+#                         'SEMIO_RAT',
+#                         'SEMIO_KRIT',
+#                         'SEMIO_DOM',
+#                         'SEMIO_KAEM',
+#                         'SEMIO_PFLICHT',
+#                         'SEMIO_TRADV',
+#                         'SHOPPER_TYP',
+#                         'VERS_TYP',
+#                         'ZABEOTYP',
+#                         'CJT_KATALOGNUTZER',
+#                         'CJT_TYP_1',
+#                         'CJT_TYP_2',
+#                         'CJT_TYP_3',
+#                         'CJT_TYP_4',
+#                         'CJT_TYP_5',
+#                         'CJT_TYP_6',
+#                         'KBA13_SEG_MINIWAGEN',
+#                         'KBA13_CCM_2000',
+#                         'KBA13_KMH_0_140',
+#                         'KBA13_SEG_SONSTIGE',
+#                         'KBA13_VORB_0',
+#                         'KBA13_CCM_1200',
+#                         'PLZ8_ANTG2',
+#                         'KBA13_KMH_211',
+#                         'KBA13_CCM_1000',
+#                         'PLZ8_ANTG3',
+#                         'KBA13_VORB_1_2',
+#                         'KBA13_ALTERHALTER_61',
+#                         'KBA13_ALTERHALTER_30',
+#                         'KBA13_AUTOQUOTE',
+#                         'KBA13_SEG_KLEINWAGEN',
+#                         'KBA13_CCM_1600',
+#                         'KBA13_GBZ',
+#                         'KBA13_SEG_WOHNMOBILE',
+#                         'KBA13_CCM_2500',
+#                         'KBA13_SEG_GROSSRAUMVANS',
+#                         'KBA13_SEG_OBEREMITTELKLASSE',
+#                         'KBA13_KRSAQUOT',
+#                         'PLZ8_GBZ',
+#                         'KBA13_VORB_2',
+#                         'KBA13_SEG_OBERKLASSE',
+#                         'KBA13_BJ_2008',
+#                         'KBA13_ALTERHALTER_60',
+#                         'PLZ8_ANTG1',
+#                         'KBA13_SEG_MITTELKLASSE',
+#                         'KBA13_SEG_GELAENDEWAGEN',
+#                         'KBA13_BJ_2000',
+#                         'KBA13_KRSSEG_KLEIN',
+#                         'KBA13_CCM_3000',
+#                         'KBA13_SEG_UTILITIES',
+#                         'KBA13_SEG_MINIVANS',
+#                         'KBA13_VORB_1',
+#                         'KBA13_BJ_1999',
+#                         'PLZ8_ANTG4',
+#                         'KBA13_KRSSEG_OBER',
+#                         'KBA13_CCM_3001',
+#                         'KBA13_KRSSEG_VAN',
+#                         'KBA13_KRSZUL_NEU',
+#                         'KBA13_SEG_KOMPAKTKLASSE',
+#                         'KBA13_KMH_140_210',
+#                         'KBA13_SEG_SPORTWAGEN',
+#                         'KBA13_CCM_1400',
+#                         'KBA13_BAUMAX',
+#                         'PLZ8_BAUMAX',
+#                         'PLZ8_HHZ',
+#                         'KBA13_BJ_2009',
+#                         'KBA13_CCM_2501',
+#                         'KBA13_VORB_3',
+#                         'KBA13_CCM_1500',
+#                         'KBA13_BJ_2006',
+#                         'KBA13_HHZ',
+#                         'KBA13_CCM_1800',
+#                         'KBA13_SEG_KLEINST',
+#                         'KBA13_ALTERHALTER_45',
+#                         'KBA13_SEG_VAN',
+#                         'KBA13_BJ_2004',
+#                         'BALLRAUM',
+#                         'EWDICHTE',
+#                         'INNENSTADT',
+#                         'GEBAEUDETYP_RASTER',
+#                         'KKK',
+#                         'MOBI_REGIO',
+#                         'ONLINE_AFFINITAET']
 
 # Based on the 02 Notebook, contains columns selected in subset
-CENSUS_VAR_TYPES =  pd.read_csv('data/trusted/census_var_types.csv')
+var_attributes =  pd.read_excel('data/raw/DIAS Attributes - Values 2017.xlsx',
+                               header = 1,
+                                 usecols = 'B:F',
+                                 dtype = str,
+                                 sheet_name = 'Tabelle1_fixed')
+
+CENSUS_VAR_TYPES =  var_attributes[['Attribute','Type']].dropna()
 
 CENSUS_VAR_TYPES.loc[CENSUS_VAR_TYPES['Attribute'] == 'ANZ_HH_TITEL','Type'] = 'binary'
 
-def generate_nan_map(values_path = VALUES_PATH):
+def generate_nan_map(var_attributes = var_attributes):
 
     '''
     Generates the a dictionary with the list of values by columns to be replaced by NaN in a Pandas friendly format.
@@ -192,7 +198,7 @@ def generate_nan_map(values_path = VALUES_PATH):
     :return nan_val_map: Dictionary to be used in replacement
     '''
 
-    nan_val_df = pd.read_excel('data/raw/DIAS Attributes - Values 2017.xlsx', header = 1, usecols = 'B:F', dtype = str)
+    nan_val_df = var_attributes.copy()
 
     nan_val_df[['Attribute','Description']] = nan_val_df[['Attribute','Description']].fillna(method='ffill')
 
@@ -213,11 +219,11 @@ def generate_nan_map(values_path = VALUES_PATH):
         nested_nan_map[k] = {int(digit):np.nan for digit in v}
     
     # Manually setting columns that have other name in Data
-    nested_nan_map['SOHO_KZ'] = {-1:np.nan}
+    # nested_nan_map['SOHO_KZ'] = {-1:np.nan}
 
-    nested_nan_map['KK_KUNDENTYP'] = {-1:np.nan}
+    # nested_nan_map['KK_KUNDENTYP'] = {-1:np.nan}
 
-    nested_nan_map['KBA13_CCM_1401_2500'] = {-1:np.nan}
+    # nested_nan_map['KBA13_CCM_1401_2500'] = {-1:np.nan}
 
     return nested_nan_map
 
@@ -235,7 +241,7 @@ def replace_nans(dataframe):
     dataframe = dataframe.replace(nested_nan_map)
 
     # Specific Case
-    dataframe['CAMEO_DEU_2015'] = dataframe['CAMEO_DEU_2015'].replace('-1',np.nan)
+    # dataframe['CAMEO_DEU_2015'] = dataframe['CAMEO_DEU_2015'].replace('-1',np.nan)
 
     return dataframe
 
@@ -266,16 +272,27 @@ def drop_empty_rows(dataframe, threshold = 0.2):
 
 def reencode_d19_columns(dataframe):
 
-    # Grid columns that remained in the subset
-    grid_cols_list = ['D19_BANKEN_DIREKT',
-                    'D19_BANKEN_GROSS',
-                    'D19_BANKEN_LOKAL',
-                    'D19_BANKEN_REST',
-                    'D19_BIO_OEKO',
-                    'D19_DIGIT_SERV',
-                    'D19_LEBENSMITTEL',
-                    'D19_VOLLSORTIMENT',
-                    'D19_VERSAND_REST']
+    # Identifying columns automatically
+    grid_cols_list = []
+
+    d19_household_cols_list = []
+
+    for col in dataframe.columns:
+
+        # Skipping nominal columns
+        if col in ['D19_LETZTER_KAUF_BRANCHE_RZ','D19_KONSUMTYP','D19_KUNDENTYP']:
+
+            continue
+
+        if col.startswith('D19'):
+
+            if ('DATUM' in col) or ('ANZ' in col) or ('QUOTE' in col):
+
+                d19_household_cols_list.append(col)
+            
+            else:
+
+                grid_cols_list.append(col)
     
     for col in grid_cols_list:
 
@@ -287,16 +304,6 @@ def reencode_d19_columns(dataframe):
         grid_choices = [0,1,2,3]
 
         dataframe[col] = np.select(grid_conditions, grid_choices, default = np.nan)
-
-    d19_household_cols_list = ['D19_BANKEN_DATUM',
-                                'D19_BANKEN_ONLINE_QUOTE_12',
-                                'D19_GESAMT_ANZ_12',
-                                'D19_GESAMT_DATUM',
-                                'D19_GESAMT_ONLINE_QUOTE_12',
-                                'D19_KONSUMTYP',
-                                'D19_VERSAND_ANZ_12',
-                                'D19_VERSAND_DATUM',
-                                'D19_VERSAND_ONLINE_QUOTE_12']
     
     for col in d19_household_cols_list:
 
